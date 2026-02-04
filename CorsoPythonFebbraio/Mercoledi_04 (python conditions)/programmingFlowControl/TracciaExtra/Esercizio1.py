6961e9f188e80532cdb3cda1c284063287d5e3d3
Stefano Romanelli <53661102+RayCatcherS@users.noreply.github.com>
2026-02-04 18:33:18 +0100

    Create Esercizio1.py

---
+++ CorsoPythonFebbraio/Mercoledi_04 (python conditions)/programmingFlowControl/TracciaExtra/Esercizio1.py
@@ -0,0 +1,21 @@
+"""
+Scenario: Devi scrivere un programma Python che simuli un sistema di login. Il sistema deve permettere all'utente di
+inserire un nome utente e una password. Poi, deve verificare se la combinazione di nome utente e password è corretta.
+Per semplicità, puoi hardcodare nel codice una coppia di nome utente e password che sia considerata corretta.
+Requisiti:
+
+1. Input dell'Utente:
+o Il programma chiede all'utente di inserire il nome utente.
+o Poi, chiede all'utente di inserire la password.
+2. Verifica delle Credenziali:
+o Il programma controlla se il nome utente e la password inseriti corrispondono a quelli predefiniti.
+o Puoi decidere di avere le credenziali hardcoded nel codice per questo esercizio. Ad esempio, puoi usare "admin"
+come nome utente e "12345" come password.
+3. Output del Programma:
+o Se il nome utente e la password sono corretti, stampa un messaggio di benvenuto.
+Altrimenti, informa l'utente che le credenziali sono errate.
+4. Modifica dati del Programma:
+· Inserisci una condizione interna che si occupi di cambiare un dato specifico tra quelli inseriti
+Appena loggato fai scegliere fra due opzioni di domanda segreta e la risposta ( qual'è il colore preferito, quale
+animale preferito )
+"""
