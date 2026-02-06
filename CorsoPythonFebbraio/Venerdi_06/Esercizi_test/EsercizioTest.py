9cb59c8c43fa49ac040cca3359cf8d6281abe903
Stefano Romanelli <53661102+RayCatcherS@users.noreply.github.com>
2026-02-06 11:24:37 +0100

    push esercizio 1 del test

---
+++ CorsoPythonFebbraio/Venerdi_06/Esercizi_test/EsercizioTest.py
@@ -0,0 +1,29 @@
+
+"""
+Esercizio 1:  Condizioni e cicli
+Chieda all’utente di inserire un numero intero positivo. 
+ 
+ Usi un ciclo for per stampare tutti i numeri da 1 fino al numero inserito. 
+ 
+ Per ogni numero: 
+ 
+ stampi "pari" se il numero è pari 
+ 
+ stampi "dispari" se il numero è dispari 
+ 
+ 
+ 
+ Se l’utente inserisce un numero minore o uguale a zero, il programma deve stampare un messaggio di errore.
+ 
+"""
+
+n = int(input("inserisci un numero intero positivo\n"))
+
+if(n <= 0):
+    print("Errore")
+else:
+    for i in range(1, n + 1):
+        if(i%2 == 0):
+            print(i, "è pari")
+        else:
+            print(i, "è dispari")
